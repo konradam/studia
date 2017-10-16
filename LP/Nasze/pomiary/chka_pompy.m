4d87d378bff65e761cebd88669955c2faa11f8cb
Konrad Adamczyk <konrad.adamczyk@medapp.pl>
2017-10-16 19:17:35 +0200

    ch-ka pompy i zaworu gornego

---
+++ LP/Nasze/pomiary/chka_pompy.m
@@ -0,0 +1,34 @@
+clear all;
+close all;
+
+% p1 = [[215.917508417508 -485.517316017315 478.461604136604 -68.0667460317460]];
+
+PWM_level = .2:.1:1;
+pred_napelniania = [8.69 39.9 59.52 75.97 89.99 102.46 115.05 128.27 139.79];
+
+figure()
+plot(PWM_level, pred_napelniania, 'r*');
+hold on;
+p1 = polyfit(PWM_level, pred_napelniania, 3);
+osx1 = linspace(min(PWM_level),max(PWM_level),1000);
+f1 = polyval(p1, osx1);
+plot(osx1,f1);
+axis([.2 1 min(pred_napelniania)-1,max(pred_napelniania)+1]);
+xlabel('Wspó³czynnik wype³nienia PWM');
+ylabel('Wydajnoœæ pompy [cm^3/s]');
+legend('Wartoœci zmierzone', 'Aproksymacja')
+hold off;
+
+
+%% napelnienia pomp¹ PWM = 1
+
+load('pompa_pwm_1.mat', 'TankLevel');
+
+figure()
+poziom = TankLevel.signals.values(:,1);
+czas = TankLevel.time(:,1);
+plot(czas, poziom,'b');
+axis([0 max(czas) 0 35]);
+xlabel('Czas [s]');
+ylabel('Poziom zbiornika [cm]');
+
