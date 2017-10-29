b3a1ba7091aa9f44afeb257a84eddb58590e420a
Konrad Adamczyk <konrad.adamczyk@medapp.pl>
2017-10-29 12:10:12 +0100

    nastawy LQR

--- LP/Nasze/LQR/SzukanieNastaw.m
+++ LP/Nasze/LQR/SzukanieNastaw.m
@@ -24,7 +24,12 @@ h0 = 20;
 
 xr = 20;
 ur = [0.254860012262847 0.775110454488783];
+% ur = [ 0.775110454488783 0.254860012262847];
 zaworPWM = ur(2);
 pompaPWM = ur(1);
 
-[A B C D] = linmod('modelZbiornik2', xr, ur);
+[A B C D] = linmodv5('modelZbiornik2', xr, ur);
+
+Q = 10000;
+R = [10 0; 0 10];
+K = lqr(A,B,Q,R);
