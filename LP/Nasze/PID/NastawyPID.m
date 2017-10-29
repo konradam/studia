a8bcffb65725d11ec4c7cf94b472e7bbf20bb309
Konrad Adamczyk <konrad.adamczyk@medapp.pl>
2017-10-29 13:25:34 +0100

    refactoring model_histereza i modelLQR_histereza, zrobienie histerezy

--- LP/Nasze/PID/NastawyPID.m
+++ LP/Nasze/PID/NastawyPID.m
@@ -1,4 +1,7 @@
 %% wskaznik jakosci - uchybu
+clear all;
+close all;
+
 P1 = 1.089062371871661;
 I1 = .002562814996863;
 D1 = -0.001112798115117;
@@ -7,12 +10,13 @@ I2 = 0;
 D2 = -0.189641732763578;
 
 T_sim = 300;
-h0 = 30;
-ref = 5;
+h0 = 15;
+ref = 10;
 load('parametryWielomianuPWMPompy.mat')
 load('parametryWielomianuPWMZaworu.mat')
 load('parametryWielomianuC1Zaworu.mat')
 load('parametryWielomianuC2Zaworu.mat')
+pompaP1 = p1;       
 
 %% wskaznik jakosci - czas symulacji
 
